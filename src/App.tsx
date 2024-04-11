42277e672092bb5410ec07c3d592f81a2f98c615
Thesly Silva <thesley456silva@gmail.com>
2024-04-11 17:33:05 -0300

    Testando Monetizacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
 import "./App.css";
 
 function App() {
-    const produtos = [
-        { titulo: "Red", id: 1 },
-        { titulo: "Yellow", id: 2 },
-        { titulo: "Orange", id: 3 },
-        { titulo: "Blue", id: 4 },
-        { titulo: "Green", id: 5 }
-    ];
-
-    const lista = produtos.map(produto => (
-        <li key={produto.id}>{produto.titulo}</li>
-    ));
-
     return (
         <>
-            <h1>Teste</h1>
-            <ol>{lista}</ol>
+            <h1>Ola Mundo</h1>
         </>
     );
 }
